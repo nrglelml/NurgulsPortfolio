4180e47998e1ac8ba4bdd3aaf122fc36da02efe7
Nurgul Elmali <101966469+nrglelml@users.noreply.github.com>
2026-06-30 13:44:48 +0300

    skills_page

---
+++ DTOLayer/PagedResultDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOLayer
+{
+    public class PagedResultDTO<T>
+    {
+        public List<T> Items { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+
+        public bool HasPreviousPage => CurrentPage > 1;
+        public bool HasNextPage => CurrentPage < TotalPages;
+    }
+}
---
+++ NurgulsPortfolio/Areas/Admin/Controllers/SkillController.cs
@@ -0,0 +1,73 @@
+﻿using BusinessLayer.Abstract;
+using DTOLayer;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NurgulsPortfolio.Areas.Admin.Controllers
+{
+    public class SkillController : BaseAdminController
+    {
+        private readonly ISkillService _skillService;
+
+        public SkillController(ISkillService skillService)
+        {
+            _skillService = skillService;
+        }
+
+        public IActionResult Index(int page = 1)
+        {
+            int pageSize = 4; 
+
+            var allSkills = _skillService.TGetList(); 
+            int totalCount = allSkills.Count;
+            int totalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+
+            var pagedSkills = allSkills
+                .Skip((page - 1) * pageSize) 
+                .Take(pageSize)              
+                .ToList();
+
+            var result = new PagedResultDTO<Skill>
+            {
+                Items = pagedSkills,
+                CurrentPage = page,
+                TotalPages = totalPages,
+                PageSize = pageSize,
+                TotalCount = totalCount
+            };
+
+            return View(result);
+        }
+        [HttpPost]
+        public IActionResult AddSkill(Skill p)
+        {
+            p.IsActive = true;
+            _skillService.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult MakeActive(int id)
+        {
+           var value= _skillService.TGetByID(id);
+            value.IsActive = true;
+            _skillService.TUpdate(value);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult MakePassive(int id)
+        {
+            var value = _skillService.TGetByID(id);
+            value.IsActive = false;
+            _skillService.TUpdate(value);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult EditSkill(Skill p)
+        {
+            var skill = _skillService.TGetByID(p.Id);
+            skill.SkillTitle = p.SkillTitle;
+            _skillService.TUpdate(skill);
+            return RedirectToAction("Index");
+        }
+    }
+}
