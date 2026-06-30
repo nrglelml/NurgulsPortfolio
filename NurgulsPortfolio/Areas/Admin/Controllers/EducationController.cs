6a5c47c00d7fcd48053b4663f4f5b4f09e3799a6
Nurgul Elmali <101966469+nrglelml@users.noreply.github.com>
2026-06-30 19:36:17 +0300

    services_page

---
+++ NurgulsPortfolio/Areas/Admin/Controllers/EducationController.cs
@@ -0,0 +1,107 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.ValidationRules;
+using DTOLayer;
+using EntityLayer.Concrete;
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NurgulsPortfolio.Areas.Admin.Controllers
+{
+    public class EducationController : BaseAdminController
+    {
+        private readonly IEducationService _educationService;
+
+        public EducationController(IEducationService educationService)
+        {
+            _educationService = educationService;
+        }
+
+        public IActionResult Index(int page = 1)
+        {
+            int pageSize = 4;
+
+            var allItems = _educationService.TGetList();
+            int totalCount = allItems.Count;
+            int totalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+
+            var pagedSkills = allItems
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            var result = new PagedResultDTO<Education>
+            {
+                Items = pagedSkills,
+                CurrentPage = page,
+                TotalPages = totalPages,
+                PageSize = pageSize,
+                TotalCount = totalCount
+            };
+
+            return View(result);
+        }
+        [HttpPost]
+        public IActionResult AddEducation(Education p)
+        {
+            p.IsActive = true;
+            EducationValidator validationRules = new EducationValidator();
+            ValidationResult result = validationRules.Validate(p);
+
+            if (result.IsValid)
+            {
+                _educationService.TAdd(p);
+                return RedirectToAction("Index", "Education", new { area = "Admin" });
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View(p);
+            }
+        }
+        [HttpPost]
+        public IActionResult MakeActive(int id)
+        {
+            var value = _educationService.TGetByID(id);
+            value.IsActive = true;
+            _educationService.TUpdate(value);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult MakePassive(int id)
+        {
+            var value = _educationService.TGetByID(id);
+            value.IsActive = false;
+            _educationService.TUpdate(value);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult EditEducation(Education p)
+        {
+            var education = _educationService.TGetByID(p.Id);
+
+            EducationValidator validationRules = new EducationValidator();
+            ValidationResult result = validationRules.Validate(p);
+
+            if (!result.IsValid)
+            {
+                foreach (var item in result.Errors)
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                return RedirectToAction("Index");
+            }
+
+            education.School = p.School;
+            education.Department = p.Department;
+            education.Degree=p.Degree;
+            education.GPA = p.GPA;
+            education.StartDate = p.StartDate;
+            education.EndDate = p.EndDate;
+            _educationService.TUpdate(education);
+            
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ NurgulsPortfolio/Areas/Admin/Controllers/ServiceController.cs
@@ -0,0 +1,145 @@
+﻿using AutoMapper;
+using BusinessLayer.Abstract;
+using BusinessLayer.ValidationRules;
+using DTOLayer;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NurgulsPortfolio.Areas.Admin.Controllers
+{
+    public class ServiceController : BaseAdminController
+    {
+        private readonly IMyService _myService;
+        private readonly IMapper _mapper;
+
+        public ServiceController(IMyService myService, IMapper mapper)
+        {
+            _myService = myService;
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _myService.TGetList();
+            var dtoList = _mapper.Map<List<ServiceAddEditDTO>>(values);
+            return View(dtoList);
+        }
+        [HttpPost]
+        public IActionResult AddService(ServiceAddEditDTO p)
+        {
+             p.IsActive = true;
+             var service = _mapper.Map<Service>(p);
+            if (p.ImageFile != null)
+            {
+                if (!string.IsNullOrEmpty(p.ImageURL))
+                {
+                    var oldPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot",
+                                  p.ImageURL.TrimStart('/'));
+                    if (System.IO.File.Exists(oldPath))
+                        System.IO.File.Delete(oldPath);
+                }
+                service.ImageURL = SaveImageFile(p.ImageFile);
+            }
+
+            ServiceValidator validationRules=new ServiceValidator();
+             ValidationResult result = validationRules.Validate(service);
+
+            if (result.IsValid)
+            {
+                _myService.TAdd(service);
+                return RedirectToAction("Index", "Service", new { area = "Admin" });
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return RedirectToAction("Index", "Service", new { area = "Admin" });
+            }
+        }
+        [HttpPost]
+        public IActionResult MakeActive(int id)
+        {
+            var value = _myService.TGetByID(id);
+            value.IsActive = true;
+            _myService.TUpdate(value);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult MakePassive(int id)
+        {
+            var value = _myService.TGetByID(id);
+            value.IsActive = false;
+            _myService.TUpdate(value);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult EditService(ServiceAddEditDTO p)
+        {
+            var service=_mapper.Map<Service>(p);
+            if (p.ImageFile != null)
+            {
+                if (!string.IsNullOrEmpty(p.ImageURL))
+                {
+                    var oldPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot",
+                                  p.ImageURL.TrimStart('/'));
+                    if (System.IO.File.Exists(oldPath))
+                        System.IO.File.Delete(oldPath);
+                }
+                service.ImageURL = SaveImageFile(p.ImageFile);
+            }
+            else
+            {
+                service.ImageURL = p.ImageURL;
+            }
+
+            ServiceValidator validationRules = new ServiceValidator();
+            ValidationResult result = validationRules.Validate(service);
+
+            if (result.IsValid)
+            {
+                _myService.TUpdate(service);
+                return RedirectToAction("Index", "Service", new { area = "Admin" });
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return RedirectToAction("Index", "Service", new { area = "Admin" });
+            }
+        
+        }
+        [HttpPost]
+        public IActionResult DeleteService(int id)
+        {
+           var value= _myService.TGetByID(id);
+            _myService.TDelete(value);
+            return RedirectToAction("Index", "Service", new { area = "Admin" });
+        }
+
+        private string SaveImageFile(IFormFile imageFile)
+        {
+            var resource = Directory.GetCurrentDirectory();
+            var extension = Path.GetExtension(imageFile.FileName);
+            var imagename = Guid.NewGuid() + extension;
+            var folder = Path.Combine(resource, "wwwroot", "serviceImages");
+
+            if (!Directory.Exists(folder))
+                Directory.CreateDirectory(folder);
+
+            var savelocation = Path.Combine(folder, imagename);
+
+            using (var stream = new FileStream(savelocation, FileMode.Create))
+            {
+                imageFile.CopyTo(stream);
+            }
+
+            return "/serviceImages/" + imagename;  
+        }
+    }
+}
