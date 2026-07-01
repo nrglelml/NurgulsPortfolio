30c576674898950b7b6a0d42be7a8fd5c08ebe15
Nurgul Elmali <101966469+nrglelml@users.noreply.github.com>
2026-07-01 16:07:36 +0300

    admin_testimonials_page

---
+++ NurgulsPortfolio/Areas/Admin/Controllers/TestimonialController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.ValidationRules;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NurgulsPortfolio.Areas.Admin.Controllers
+{
+    public class TestimonialController :BaseAdminController
+    {
+        private readonly ITestimonialService _testimonialService;
+
+        public TestimonialController(ITestimonialService testimonialService)
+        {
+            _testimonialService = testimonialService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _testimonialService.TGetList();
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var values = _testimonialService.TGetByID(id);
+            _testimonialService.TDelete(values);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ NurgulsPortfolio/Controllers/TestimonialController.cs
@@ -0,0 +1,42 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.ValidationRules;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NurgulsPortfolio.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        private readonly ITestimonialService _testimonialService;
+
+        public TestimonialController(ITestimonialService testimonialService)
+        {
+            _testimonialService = testimonialService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddTestimonial(Testimonials p)
+        {
+            TestimonialValidator validationRules = new TestimonialValidator();
+            ValidationResult result = validationRules.Validate(p);
+
+            if (result.IsValid)
+            {
+                _testimonialService.TAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
