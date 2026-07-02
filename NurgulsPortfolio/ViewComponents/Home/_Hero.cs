44db2c617054de6beb3ed60019104c2165c3e14c
Nurgul Elmali <101966469+nrglelml@users.noreply.github.com>
2026-07-02 15:57:53 +0300

    home_hero_services_component

---
+++ NurgulsPortfolio/ViewComponents/Home/_Hero.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NurgulsPortfolio.ViewComponents.Home
+{
+    public class _Hero : ViewComponent
+    {
+        private readonly IAboutMeService _aboutMeService;
+        public _Hero(IAboutMeService aboutMeService)
+        {
+            _aboutMeService = aboutMeService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var values= _aboutMeService.TGetList().FirstOrDefault();
+            return View(values);
+        }
+    }
+}
---
+++ NurgulsPortfolio/ViewComponents/Home/_Services.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NurgulsPortfolio.ViewComponents.Home
+{
+    public class _Services:ViewComponent
+    {
+        private readonly IMyService _myService;
+        public _Services(IMyService myService)
+        {
+            _myService = myService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _myService.TGetListByStatus(true);
+            return View(values);
+        }
+    }
+}
