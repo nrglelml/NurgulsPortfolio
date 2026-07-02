047b7a86b839fd93d85ca01b16aee6820d4392b3
Nurgul Elmali <101966469+nrglelml@users.noreply.github.com>
2026-07-02 16:20:48 +0300

    home_projects_page

---
+++ NurgulsPortfolio/ViewComponents/Home/_Projects.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NurgulsPortfolio.ViewComponents.Home
+{
+    public class _Projects:ViewComponent
+    {
+        private readonly IProjectService _projectService;
+        public _Projects(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var values = _projectService.TGetListWithSkill().Where(x=>x.IsActive==true).ToList();
+            return View(values);
+        }
+    }
+}
