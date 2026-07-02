165add761942bae53ca7f35e7b2abd5a30b0f52a
Nurgul Elmali <101966469+nrglelml@users.noreply.github.com>
2026-07-02 16:48:40 +0300

    home_experience_component

---
+++ NurgulsPortfolio/ViewComponents/Home/_Experience.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Mvc;
+
+namespace NurgulsPortfolio.ViewComponents.Home
+{
+    public class _Experience : ViewComponent
+    {
+        private readonly IEducationService _educationService;
+        private readonly IExperienceService _experienceService;
+
+        public _Experience(IEducationService educationService, IExperienceService experienceService)
+        {
+            _educationService = educationService;
+            _experienceService = experienceService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.Education = _educationService.TGetListByStatus(true).OrderByDescending(x => x.StartDate).ToList();
+            var values = _experienceService.TGetListByStatus(true).OrderByDescending(x => x.StartDate).ToList();
+            return View(values);
+        }
+    }
+}
