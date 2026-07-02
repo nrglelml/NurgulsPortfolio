14db554aef28eb23a453b0cca3a29a1387b59b07
Nurgul Elmali <101966469+nrglelml@users.noreply.github.com>
2026-07-02 16:41:40 +0300

    home_testimonial_component

---
+++ NurgulsPortfolio/ViewComponents/Home/_Testimonials.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NurgulsPortfolio.ViewComponents.Home
+{
+    public class _Testimonials : ViewComponent
+    {
+        private readonly ITestimonialService _testimonialService;
+        public _Testimonials(ITestimonialService testimonialService)
+        {
+            _testimonialService = testimonialService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var values = _testimonialService.TGetList();
+            return View(values);
+        }
+    }
+}
